758292b965ad581fb0fe73f3ceb11965d7463822
Benjamin Young <ben@advolvemg.com>
2019-02-02 11:40:09 -0500

    Getting highs and lows of stock

--- methods/simple.js
+++ methods/simple.js
@@ -1,8 +1,38 @@
 const config = require('../config.json');
+const simple_config = config.methods.simple;
 const Alpaca = require('@alpacahq/alpaca-trade-api');
 
 const alpaca = new Alpaca(config.alpaca);
 
-alpaca.getAccount().then(function(account) {
-  console.log('Current Account:', account)
+alpaca.getClock().then(function(clock) {
+  if (clock.is_open || true) {
+    simple_config.stocks.forEach(function(stock) {
+      alpaca.getAsset(stock).then(function(asset) {
+        if (asset.tradable) {
+          alpaca.getBars('day', stock, {
+            limit: 5
+          }).then(function(bars) {
+            var min = false;
+            var max = false;
+            var current = 0;
+            bars = bars[stock];
+
+            bars.forEach(function(bar) {
+              if (!min || min > bar.l) {
+                min = bar.l;
+              }
+
+              if (!max || max < bar.h) {
+                max = bar.h;
+              }
+
+              current = bar.c
+            });
+
+
+          });
+        }
+      });
+    });
+  }
 });
